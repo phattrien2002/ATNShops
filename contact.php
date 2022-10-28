bfdcaac445772a40f37b32da9d05190ca399f0ac
phattrien2002 <trientpgcc200063@fpt.edu.vn>
2022-10-28 16:08:49 +0700

    ok

--- contact.php
+++ contact.php
@@ -88,7 +88,7 @@
                     <div class="contact__widget">
                         <span class="icon_mail_alt"></span>
                         <h4>Email</h4>
-                        <p>sieungcc19186@fpt.edu.vn</p>
+                        <p>trientpgcc200063@fpt.edu.vn</p>
                     </div>
                 </div>
             </div>
